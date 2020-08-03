224f3017767edc830494d1157a998de1505fe1a3
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-08-03 12:19:58 +0530

    working example with pug

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const users = [];
+
 app.set('view engine', 'pug');
 app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
   res.render('index', { pageTitle: "Add User" });
 });
 
 app.get('/users', (req, res, next) => {
-  res.render('users', { pageTitle: 'Users ' });
+  res.render('users', { pageTitle: 'Users', users: users });
 });
 
 app.post('/users', (req, res, next) => {
+  users.push({ name: req.body.name });
   res.redirect('/users');
 });
 
