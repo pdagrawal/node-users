8cb5d593501000331bcad20e625dd4a984820d1d
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-08-03 12:11:51 +0530

    Added templates in pug

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.get('/', (req, res, next) => {
-  res.render();
+  res.render('index', { pageTitle: "Add User" });
 });
 
 app.get('/users', (req, res, next) => {
-  res.render();
+  res.render('users', { pageTitle: 'Users ' });
 });
 
 app.post('/users', (req, res, next) => {
