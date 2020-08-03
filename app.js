8ae22666d05e0aad7851a86649b12eb29103352b
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-08-03 13:53:27 +0530

    solution with ejs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const users = [];
 
 app.engine('hbs', expresshbs({ defaultLayout: 'main', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
